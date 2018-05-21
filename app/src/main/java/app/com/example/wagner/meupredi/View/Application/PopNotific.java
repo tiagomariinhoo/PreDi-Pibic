10ca9ec428cc5b5ded7f7b9bb8ca80b65888c0d9
wagnerfilho1995 <wagnerfilho1995@gmail.com>
2018-05-21 18:38:29 -0300

    Perfil - Refatoramento

---
+++ app/src/main/java/app/com/example/wagner/meupredi/View/Application/PopNotific.java
@@ -0,0 +1,32 @@
+package app.com.example.wagner.meupredi.View.Application;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import app.com.example.wagner.meupredi.Model.ModelClass.Paciente;
+import app.com.example.wagner.meupredi.R;
+
+public class PopNotific extends Activity {
+
+    private ListView listaDeNotificacoes;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.pop_notific);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) ( width*.8), (int) (height*.50));
+
+    }
+}
