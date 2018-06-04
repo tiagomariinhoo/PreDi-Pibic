9e607e4c44613ddbc21511471c5aff99e239506b
wagnerfilho1995 <wagnerfilho1995@gmail.com>
2018-06-03 23:26:50 -0300

    Refatoramento - Grafico Peso

--- app/src/main/java/app/com/example/wagner/meupredi/View/Application/MainViews/GraficosPeso.java
+++ app/src/main/java/app/com/example/wagner/meupredi/View/Application/MainViews/GraficosPeso.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import java.util.ArrayList;
@@ -33,15 +38,37 @@ public class GraficosPeso extends AppCompatActivity{
         mChart.setDragEnabled(true);
         mChart.setScaleEnabled(false);
 
+        LimitLine upper_limit = new LimitLine(65f, "Danger");
+        upper_limit.setLineWidth(4f);
+        upper_limit.enableDashedLine(10f, 10f, 0f);
+        upper_limit.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
+        upper_limit.setTextSize(15f);
+
+        LimitLine lower_limit = new LimitLine(65f, "Too low");
+        upper_limit.setLineWidth(4f);
+        upper_limit.enableDashedLine(10f, 10f, 0f);
+        upper_limit.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
+        upper_limit.setTextSize(15f);
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.removeAllLimitLines();
+        leftAxis.addLimitLine(upper_limit);
+        leftAxis.addLimitLine(lower_limit);
+        leftAxis.setAxisMaximum(100f);
+        leftAxis.setAxisMinimum(25f);
+        leftAxis.enableAxisLineDashedLine(10f, 10f, 0);
+        leftAxis.setDrawLimitLinesBehindData(true);
+
         ArrayList<Entry> yValues = new ArrayList<>();
 
-        yValues.add(new Entry(0, 60f));
         yValues.add(new Entry(1, 50f));
         yValues.add(new Entry(2, 70f));
         yValues.add(new Entry(3, 30f));
         yValues.add(new Entry(4, 50f));
         yValues.add(new Entry(5, 60f));
         yValues.add(new Entry(6, 65f));
+
+        mChart.getAxisRight().setEnabled(false);
 
         LineDataSet set1 = new LineDataSet(yValues, "Data Set 1");
 
@@ -54,5 +81,24 @@ public class GraficosPeso extends AppCompatActivity{
         LineData data = new LineData(dataSets);
 
         mChart.setData(data);
+
+        String[] values = new String[]{"Jan", "Fev", "Mar", "Apr", "May", "Jun", "Jul"};
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setValueFormatter(new MyXAxisValueFormatter(values));
+ //       xAxis.setGranularity(1f);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
+    }
+
+    public class MyXAxisValueFormatter implements IAxisValueFormatter{
+        private String[] mValues;
+        public MyXAxisValueFormatter(String[] values){
+            this.mValues = values;
+        }
+        @Override
+        public String getFormattedValue(float value, AxisBase axis){
+            return mValues[(int)value];
+
+        }
     }
 }
