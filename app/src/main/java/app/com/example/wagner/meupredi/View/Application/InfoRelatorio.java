8487ac494e3910f7ed54fa7fa28385f725669329
fabriond <gfbf@ic.ufal.br>
2018-12-03 00:50:46 -0300

    Adicionada animação no tutorial da inferência

--- app/src/main/java/app/com/example/wagner/meupredi/View/Application/InfoRelatorio.java
+++ app/src/main/java/app/com/example/wagner/meupredi/View/Application/InfoRelatorio.java
@@ -2,6 +2,7 @@ package app.com.example.wagner.meupredi.View.Application;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import app.com.example.wagner.meupredi.Model.ModelClass.Paciente;
@@ -22,7 +23,7 @@ public class InfoRelatorio extends AppCompatActivity {
         paciente = PacienteUpdater.getPaciente();//(Paciente) getIntent().getExtras().get("Paciente");
 
         msgBalao = findViewById(R.id.msg_tutorial_relatorio_info);
-        variavelAtual = findViewById(R.id.text_variavel_relatorio);
+        //variavelAtual = findViewById(R.id.text_variavel_relatorio);
 
         String mensagem = "Inicialmente, o sistema precisa analisar sua taxa de Glicemia em Jejum," +
                 " de acordo com o valor dela, vamos definir o próximo passo.";
@@ -30,9 +31,26 @@ public class InfoRelatorio extends AppCompatActivity {
         msgBalao.setText(mensagem);
 
         String nomeVariavelAtual = "Glicose em Jejum";
-        variavelAtual.setText(nomeVariavelAtual);
+        //variavelAtual.setText(nomeVariavelAtual);
 
         double glicoseJejum = paciente.getGlicoseJejum();
 
+    }
+
+    public void onCardClick(View view) {
+        flipCard();
+    }
+
+    private void flipCard() {
+        View rootLayout = findViewById(R.id.main_activity_card);
+        View cardFace = findViewById(R.id.main_activity_card_front);
+        View cardBack = findViewById(R.id.main_activity_card_back);
+
+        FlipAnimation flipAnimation = new FlipAnimation(cardFace, cardBack);
+
+        if (cardFace.getVisibility() == View.GONE) {
+            flipAnimation.reverse();
+        }
+        rootLayout.startAnimation(flipAnimation);
     }
 }
